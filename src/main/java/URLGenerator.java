c302a2bdd6340ec992e0b0377614d780b95b5dd5
Everton Cavalcante <evertonranielly@gmail.com>
2025-09-25 14:51:59 -0300

    Initial commit

---
+++ src/main/java/URLGenerator.java
@@ -0,0 +1,8 @@
+package generation;
+
+public class URLGenerator {
+    private static final GENAI_MODEL = "gemini-2.5-flash-lite";
+    private static final API_KEY = "AIzaSyCSTIRody9L1LYpCKuhHNXxMAuNO6e6Lts";
+
+
+}
